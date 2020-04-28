59aeca3830eb7dc00b5bc1de8d92af941f4fc980
decent-scraper-1450 <jaimealba1574@gmail.com>
2020-04-28 17:14:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
   # code goes here
+  attr_accessor :mood, :owner
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name, owner)
+    @name = name
+    @mood = "happy"
+    @owner = owner
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
